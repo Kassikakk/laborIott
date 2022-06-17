3aed68e0ff5a91b97d20acb82a73491e33fb96e9
Kassikakk <kleiger@ut.ee>
2022-06-17 19:59:37 +0300

    server response tester

--- laborIott/adapters/ZMQAdapter.py
+++ laborIott/adapters/ZMQAdapter.py
@@ -6,7 +6,7 @@ import logging
 #set up logging if needed
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 log = logging.getLogger(__name__)
-log.setLevel(logging.INFO)
+log.setLevel(logging.DEBUG)
 ch = logging.StreamHandler()
 ch.setFormatter(formatter)
 ch.setLevel(logging.DEBUG)
---
+++ laborIott/server/Tests/Srv_response.py
@@ -0,0 +1,33 @@
+import pickle
+import zmq
+
+
+
+address = "jobinyvon"
+inport = 5555
+outport = 5555
+
+
+insock, poller = zmq.Context().socket(zmq.SUB), zmq.Poller()
+insock.connect("tcp://%s:%d" % (address, inport))
+insock.setsockopt(zmq.SUBSCRIBE, b'')
+poller.register(insock, zmq.POLLIN)
+timeout = 100  # milliseconds
+repeat = int(1000 / timeout)  # global timeout / single shot timeout
+
+outsock = zmq.Context().socket(zmq.PUB)
+outsock.bind("tcp://*:%d" % outport)
+topic = "idus.echo"
+for i in range(5):
+	command = "calling {}".format(i)
+	outsock.send_serialized(command, serialize=lambda rec: (topic.encode(), pickle.dumps(rec)))
+	for j in range(repeat):
+			if poller.poll(timeout):
+				topic1, record = insock.recv_serialized(
+						deserialize=lambda msg: (msg[0].decode(), pickle.loads(msg[1])))
+				if (topic1 == topic):
+					print(record)
+				else:
+					print("midagi muud")
+					break
+	print("end of cycle {}". format(i))
