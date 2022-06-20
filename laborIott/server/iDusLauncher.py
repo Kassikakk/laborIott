3f459e332684b74892600afac48cf92ee1952079
Kassikakk <kleiger@ut.ee>
2022-06-20 16:19:51 +0300

    midagi vfeel

--- laborIott/server/iDusLauncher.py
+++ laborIott/server/iDusLauncher.py
@@ -6,6 +6,6 @@ def localPath(filename):
 	return os.path.join(os.path.dirname(os.path.abspath(__file__)),filename)
 
 devdict = {"iDus": SDKAdapter(localPath("../instruments/Andor/Inst/atmcd32d_legacy"))}
-inlist = (("172.17.202.90", 5555),)
+inlist = (("mikro-spektro", 5555),)
 svr = ZMQServer(devdict, inlist, 5555)
 svr.run()
