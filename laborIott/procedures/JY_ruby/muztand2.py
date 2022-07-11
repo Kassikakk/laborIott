64f97354e9812fb773a40d96f6c0c62b6fe02304
Kassikakk <kleiger@ut.ee>
2022-07-11 20:40:59 +0300

    New fit project

---
+++ laborIott/procedures/JY_ruby/muztand2.py
@@ -0,0 +1,57 @@
+class Wrkr(QThread): #või tavathread
+
+	def def __init__(self):
+		self.running = Event()
+		pass
+		
+	def run(self, data_queue, settings_queue):
+		paramlist = None
+		delim = None
+		sloped, cyclic = True, True
+		while(True):
+			self.startIdus.emit(True)  # reserve false for abort?
+			# wait for data arrival
+			while data_queue.empty():
+				if not self.running.is_set():
+					return
+			xData, spcData = data_queue.get(False)
+			#check param queue & set params
+			if not self.paramQueue.empty():
+				sloped, cyclic = self.paramQueue.get(False)
+				#we need to get more data here
+			# do the fitting(s)
+			
+			#how we'd like to proceed here? Something like
+			for n in range(2):
+				if fitter_active[n]:
+					fitted, params = fitter[n].run(np.array(spcData[begin[n]:end[n]]), xData[begin[n]:end[n]], paramlist, delim, sloped)
+					if fitter[n].success:
+						self.updateData.emit((n,params[0]))
+						self.updateFitShape.emit(n, tuple(xData), tuple(fitted[0]), colors[n])
+
+
+
+class Fitter(object):
+	def def __init__(self, func):
+		self.paramlist = None #previous values
+		pass
+		
+	def fit(self, data, xdata, paramlist=None, sloped, delim):
+		if paramlist is not None and cyclic:  # go cyclic if possible
+				delim = (paramlist[0] + paramlist[3]) / 2
+			else:
+				paramlist = None
+				delim = None  # võetakse skaala keskpunkt
+
+			if sloped:
+				if paramlist is not None and len(paramlist) == 7:
+					paramlist += [0.0]
+				fitted, params = fit_DLorentz_slope(np.array(spcData), xData, paramlist													paramlist)  # eraldusx maksimumide vahel
+				paramlist = [params[0][i * 2] for i in range(8)]
+			else:
+				if paramlist is not None and len(paramlist) == 8:
+					paramlist = paramlist[:7]
+				fitted, params = fit_DLorentz(np.array(spcData), xData, delim, paramlist)
+				paramlist = [params[0][i * 2] for i in range(7)]
+		return func(data, xdata, paramlist)
+	
