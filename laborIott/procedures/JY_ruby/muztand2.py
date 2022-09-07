98d89f33eed6fa11ca4bc5fff55759047d5f34f9
Kassikakk <kleiger@ut.ee>
2022-09-07 11:44:03 +0300

    Proc advanced a little

--- laborIott/procedures/JY_ruby/muztand2.py
+++ laborIott/procedures/JY_ruby/muztand2.py
@@ -2,22 +2,27 @@
 from fitter import Fitter
 
 
+class FitContainer(object): # combines Fitter with a few variables relevant to the current case
+	def __init__(self):
+		self.active = True
+		self.range = [0, 1024]
+		self.sloped = True
+		self.cyclic = True
+		self.fitter = Fitter()
+
 class Wrkr(QThread): #või Thread
 	#create a dataready signal here but this requires a qobject, right? So QThread it is, still.
 
-	class FitContainer(object):
-		active = True
-		range = [0,1024]
-		sloped = True
-		cyclic = True
-		fitter = Fitter()
+
 
 	def __init__(self, startsignal, data_queue, settings_queue):
 		self.running = Event()
 		self.startsignal = startsignal
 		self.data_queue = data_queue
 		self.settings_queue = settings_queue
-		self.fitter = [Fitter(), Fitter()]
+		self.numFitters = 2
+		self.fitters = [FitContainer()] * self.numFitters
+
 
 
 	def stop(self):
@@ -39,22 +44,23 @@ class Wrkr(QThread): #või Thread
 		sloped, cyclic = True, True #all of these - handle differently (2 copies needed)
 		#well we need a whole set of parameters here
 		fitter = [Fitter(DLorentz_fit), Fitter(Lorentz_fit)] #we praably need a different init here
+
 		while(True):
 			self.startsignal.emit(True)  # well how can we do it from here?
 			# wait for data arrival
-			while data_queue.empty():
+			while self.data_queue.empty():
 				if not self.running.is_set():
 					return
 			xData, spcData = data_queue.get(False)
 			#check param queue & set params
-			if not self.settings_queue .empty():
+			if not self.settings_queue.empty():
 				sloped, cyclic = settings_queue.get(False)
 				#we need to get more data here
 				#所以 我們可能有一個 字典 (dictionary)
 			# do the fitting(s)
 			
 			#how we'd like to proceed here? Something like
-			for n in range(2):
+			for n in range(self.numFitters):
 				if fitter_active[n]:
 					if (fitter[n].run(xData[begin[n]:end[n]],np.array(spcData[begin[n]:end[n]]), paramlist) == 0):
 						#additional evaluation that data is reasonable
