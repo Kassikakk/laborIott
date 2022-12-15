a9ba25393a5ee92ffbddf363f6dd86a0a9f930ee
Kassikakk <kleiger@ut.ee>
2022-12-15 02:02:38 +0200

    A few comments

--- laborIott/instruments/Andor/Inst/shamrock.py
+++ laborIott/instruments/Andor/Inst/shamrock.py
@@ -6,3 +6,10 @@ class Shamrock(Instrument):
 	def __init__(self, adapter, **kwargs):
 		super().__init__(adapter, "Shamrock", **kwargs)
 		self.write("ShamrockInitialize('')")
+
+
+	#wavelengths
+	#centerpos NB 0 supported
+	#slit
+	#grating
+	#gratingdict
