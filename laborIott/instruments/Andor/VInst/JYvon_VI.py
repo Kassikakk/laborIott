005ea7626ffd09a0100f84522b249f8b39342a80
Kassikakk <kleiger@ut.ee>
2022-12-16 14:06:31 +0200

    Now the Ne fitting is ready for testing

--- laborIott/instruments/Andor/VInst/JYvon_VI.py
+++ laborIott/instruments/Andor/VInst/JYvon_VI.py
@@ -3,6 +3,7 @@ from laborIott.utils.fitter import Fitter
 from PyQt5 import QtWidgets
 import pyqtgraph as pg
 import numpy as np
+from scipy.interpolate import interp1d
 from laborIott.instruments.Andor.VInst.LambdaDlg import Ui_LambdaDialog  # pyuic5 generated dialog
 import os, sys
 
@@ -27,29 +28,28 @@ class JYvon_VI(Andor_VI):
 		self.dlg = QtWidgets.QDialog()
 		self.lamDlg = Ui_LambdaDialog()
 		self.lamDlg.setupUi(self.dlg)
-		self.keepNeLines = True
-		if not self.keepNeLines:
-			self.dlg.closeEvent = self.removeLines
+		self.dlg.closeEvent = self.removeLines
 
 		self.lambdaButt.setEnabled(True)
 		# any additional disablements?
 
-		self.neSpins = [self.lamDlg.NeEdit1,self.lamDlg.NeEdit2,self.lamDlg.NeEdit3]
+		self.neEdits = [self.lamDlg.NeEdit1,self.lamDlg.NeEdit2,self.lamDlg.NeEdit3]
 		self.neVals = [692.94675, 693.7664, 696.5431]
 		self.neChecks = [self.lamDlg.NeCheck1,self.lamDlg.NeCheck2,self.lamDlg.NeCheck3]
+		self.neUncerts = [0,0,0]
 		self.necolors = ['r','g','b']
 		self.nelns = []
 		# we'll try to make them appear and disappear with the dialog?
 		for i in range(3):
-			self.nelns += [pg.InfiniteLine(self.xdata[self.neSpins[i].value()])]
+			self.nelns += [pg.InfiniteLine()]
 			self.graphicsView.addItem(self.nelns[i])
-			if not self.keepNeLines:
-				self.nelns[i].setPen(None)
+			self.adjustNeLineMarker(i)
+			self.nelns[i].setPen(None)
 		#self.onCalcLambda()
 		self.onCalcFromLimb(6259.0)
 		self.lambdaButt.clicked.connect(self.setLambdaDlg)
 		for i in range(3):
-			self.neSpins[i].valueChanged.connect(lambda x, a=i: self.nelns[a].setPos(self.xdata[x]))
+			self.neEdits[i].changed.connect(lambda x, a=i: self.adjustNeLineMarker(i))
 			self.neChecks[i].clicked.connect(
 				lambda x, a=i: self.nelns[a].setPen(self.necolors[a] if self.neChecks[a].isChecked() else None))
 
@@ -64,6 +64,15 @@ class JYvon_VI(Andor_VI):
 			#self.graphicsView.addItem(self.overlays[i])
 			#self.overlays[i].setPen(None)
 
+	def adjustNeLineMarker(self, i):
+		try:
+			pos = float(self.neEdits[i].getText())
+			xval = interp1d(list(range(len(self.xdata))), self.xdata)(pos)
+		except ValueError: #either not floatable or value out of range
+			return
+		self.nelns[i].setPos(xval)
+
+
 
 	def onCalcFromLimb(self, limbval):
 		#set xarr from limb value
@@ -74,16 +83,17 @@ class JYvon_VI(Andor_VI):
 		ap = -6.26E-5
 		bp = 0.108
 		self.xdata = np.array([(limbval - (bn + bp * (512.0 - p)))/(an + ap * (512.0 - p)) for p in range(1024)])
-		#ok but we should also adjust the Ne line markers here
 		step = (self.xdata[1023] - self.xdata[0]) / 1023
 		self.lamDlg.startEdit.setText("{:.5f}".format(self.xdata[0]))
 		self.lamDlg.stepEdit.setText("{:.5f}".format(step))
 
+		#ok but we should also adjust the Ne line markers here
 		for i in range(3):
-			pos = int((self.neVals[i] -self.xdata[0]) / step )
+			#set the point values and move the Ne lines to correct position
+			pos = (self.neVals[i] -self.xdata[0]) / step 
 			if (pos > -1) and (pos < 1024):
-				self.neSpins[i].setValue(pos)
-				self.nelns[i].setPos(self.xdata[self.neSpins[i].value()])
+				self.neEdits[i].setText("{:.3f}".format(pos))
+				self.adjustNeLineMarker(i) #will it go by itself?
 
 	def fitNeLines(self):
 		#define fitter 
@@ -97,7 +107,7 @@ class JYvon_VI(Andor_VI):
 				continue
 			#set the search range; keep it within limits
 			try:
-				n1 = n2 =  int(float(self.neSpins[i].getText()))
+				n1 = n2 =  int(float(self.neEdits[i].getText()))
 			except ValueError:
 				continue
 			n1 -= srchrange
@@ -117,13 +127,17 @@ class JYvon_VI(Andor_VI):
 			fitter.paramlist = [float(nmax), 1.0, ymax, y0]
 			if (fitter.fit(list(range(n1, n2)), self.ydata[n1:n2]) == 0):
 				#fitting results are in
-				self.neSpins[i].setText("{:.3f}".format(fitter.paramlist[0]))
+				self.neEdits[i].setText("{:.3f}".format(fitter.paramlist[0]))
 				print(fitter.paramlist) #diagnostically
 				#move the line to place (maybe they will go if change acts programmatically)
 				#also record fitter.uncertlist[0]
+				self.neUncerts[i] = fitter.uncertlist[0]
 				#and if wanted, show self.xdata[n1:n2], fitter.fitted in an overlay
+				self.setOverlay(2, self.xdata[n1:n2], fitter.fitted,'r')
 				#wait a little maybe
+				sleep(0.2)
 	#you can clear the overlay here
+	self.setOverlay(2,[],[],None)
 
 
 	def onCalcLambda(self):
@@ -131,8 +145,11 @@ class JYvon_VI(Andor_VI):
 		vals = []
 		for i in range(3):
 			if self.neChecks[i].isChecked():
-				points += [self.neSpins[i].value()]
-				vals += [self.neVals[i]]
+				try:
+					points += [float(self.neEdits[i].getText())]
+					vals += [self.neVals[i]]
+				except ValueError:
+					continue
 		N = len(points)
 		if N == 0:
 			return
@@ -155,7 +172,7 @@ class JYvon_VI(Andor_VI):
 			self.lamDlg.stepEdit.setText("{:.5f}".format(step))
 			# readjust line positions
 			for i in range(3):
-				self.nelns[i].setPos(self.xdata[self.neSpins[i].value()])
+				self.adjustNeLineMarker(i) 
 
 	def setLambdaDlg(self):
 		self.dlg.show()
