587725e3ffa8b2961a61c3497b0b66cf68fed8fa
Kassikakk <kleiger@ut.ee>
2024-01-30 13:20:48 +0200

    USBIO instrument

---
+++ laborIott/instruments/Inhouse/USBIO.py
@@ -0,0 +1,25 @@
+from laborIott.instruments.instrument import Instrument
+
+from time import sleep
+
+requests = { 'REQ_ECHO':0,'REQ_SET_PINS' : 1, 'REQ_GET_PINS' : 2, 'REQ_SET_PWM_FRQ' : 3, 
+'REQ_GET_PWM_FRQ' : 4, 'REQ_SET_PWM_DUTY' : 5, 'REQ_GET_PWM_DUTY' : 6}
+
+
+class USBIO(Instrument):
+	'''
+	This ver2 class refers to V-USB connected USBIO, connecting
+
+	'''
+
+
+	def __init__(self, adapter, **kwargs):
+		super().__init__(adapter, "TiSph", **kwargs)
+
+
+
+	#what we need:
+	#bits (7) - may be wrapped
+	#servo freq
+	#servo duty
+	#(servo release?)
