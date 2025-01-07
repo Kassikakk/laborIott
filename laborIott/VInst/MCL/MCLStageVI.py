3ba2e53a43428a87af4501b128ed632367bfa0e0
Kassikakk <kleiger@ut.ee>
2025-01-07 18:38:37 +0200

    positionVI devel

---
+++ laborIott/VInst/MCL/MCLStageVI.py
@@ -0,0 +1,29 @@
+from laborIott.instruments.MCL_MicroStage.MicroStage import MCL_MicroStage
+from laborIott.VInst.PositionVI import Position_VI
+from laborIott.adapters.SDKAdapter import SDKAdapter
+from PyQt5 import QtWidgets, QtGui
+import os, sys
+
+def localPath(filename):
+	return os.path.join(os.path.dirname(os.path.abspath(__file__)),filename)
+
+class MCL_VI(Position_VI):
+
+	def __init__(self, refname = "MCL_MS", instrument = MCL_MicroStage, adapter = SDKAdapter("MicroDrive",False)):
+		super().__init__(refname, instrument, adapter,2)
+		#self.setWindowIcon(QtGui.QIcon(localPath('Andor.ico')))
+		self.setWindowTitle("MCL MicroStage")
+		#self.elDarkChk.setVisible(False)
+		self.resetEncButt.clicked.connect(lambda: self.gotoPos(None))
+		
+
+	def centerStage(self): #override
+		#move stage to extremes
+		poss = []
+		for p in [[-100,0],[0,-100],[100,0], [0,100]]:
+			self.instrum.pos = p
+			while self.stage.ismoving:
+				pass
+			poss += [self.instrum.pos]
+		self.stage.pos = [(poss[0][0] + poss[2][0])/2,(poss[1][1] + poss[3][1])/2]
+		self.posReached.clear()
--- laborIott/VInst/PositionVI.py
+++ laborIott/VInst/PositionVI.py
@@ -21,7 +21,10 @@ class Position_VI(VInst):
 		self.pos = [None]*dim #position
 		self.posReached = Event()  
 		self.posReached.set()
-		self.dsbl += [self.gotoButt, self.goDeltaButt, self.setSpeedButt, self.xEdit, self.yEdit, self.speedEdit]
+		self.dsbl += [self.gotoButt, self.goDeltaButt, self.setSpeedButt, self.xEdit, self.yEdit, self.zEdit, self.speedEdit]
+		for i,a in enumerate([self.xEdit, self.yEdit, self.zEdit]):
+			if i >= self.dim:
+				a.setVisible(False)
 
 		self.mousestep = 0.1  # mm per step
 		self.mouseX = True  # mouse direction
@@ -33,7 +36,7 @@ class Position_VI(VInst):
 		self.setSpeedButt.clicked.connect(self.setSpeed)
 		self.gotoButt.clicked.connect(lambda: self.gotoPos([self.xEdit.text(), self.yEdit.text()], False))
 		self.goDeltaButt.clicked.connect(lambda: self.gotoPos([self.xEdit.text(), self.yEdit.text()]))
-		self.resetEncButt.clicked.connect(lambda: self.gotoPos(None))
+		
 		self.centerButt.clicked.connect(self.centerStage)
 
 		self.addListButt.clicked.connect(lambda: self.addToList(self.refEdit.text(), self.stage.pos)) # add position to self.posList (reference from self.refEdit.text()
@@ -45,6 +48,17 @@ class Position_VI(VInst):
 
 	def onReconnect(self):
 		self.showPos()
+
+	def getPosFields(self):
+		postxt = [self.xEdit.text(), self.yEdit.text(),self.zEdit.text()]
+		pos = []
+		for i in range(self.dim):
+			try:
+				pos += [float(postxt[i])]
+			except ValueError:
+				#what now?
+				return None
+		return pos
 
 	
 	def onTimer(self):
@@ -60,5 +74,82 @@ class Position_VI(VInst):
 
 
 	def showPos(self):
-		formtext = ["{:.4f}	"] * self.dim
+		formtext = ("{:.4f}	" * self.dim)[:-1]
 		self.posLabel.setText(formtext.format(*self.instrum.pos))
+
+	def setSpeed(self, newSpeed):
+		if not self.posReached.is_set():
+			return
+		try: 
+			newSpeed = float(newSpeed)
+		except ValueError:
+			print("not floatable")
+			return #probably a messagebox should do here
+		self.instrum.speed = newSpeed # speed limits are checked within the instrument
+		self.speedEdit.setText("{:.1f}".format(self.instrum.speed))
+
+	def gotoPos(self, pos, relative=True):
+		if not self.posReached.is_set():
+			return
+		
+		if pos is None: #reset the encoders
+			self.instrum.pos = pos
+			self.posReached.clear() # to refresh pos
+			return
+		
+		#on the other hand, pos as a list may contain Nones
+		try:
+			pos = [None if p is None else float(p) for p in pos]
+		except ValueError:
+			print("not floatable")
+			return
+		if relative:
+			self.instrum.delta = pos
+		else:
+			self.instrum.pos = pos
+		self.posReached.clear()
+
+	def goByList(self, litem):
+		#go to pos given by a list item
+		#find key from list item
+		key = litem.text()
+		key = key[:key.find(':')]
+		#this assumes there is no ':' in the refname
+		#now get the position from dict
+		self.gotoPos(self.posDict[key], False)
+
+	def addToList(self, refName, pos):
+		# read/generate refname, record position and put them on a list
+		nameNotSet = True
+		if len(refName) == 0:
+			refName = "pos"
+		else:
+			nameNotSet = refName in self.posDict
+		if nameNotSet:
+			n = 0
+			while(True):
+				refName1 = refName + "_%d" % n
+				if refName1 in self.posDict:
+					n += 1
+				else:
+					refName = refName1
+					break
+		self.posDict[refName] = pos
+		formtext = ("{}: " + "{:.4f}, "*self.dim)[:-1]
+		self.posList.addItem(QtWidgets.QListWidgetItem(formtext.format(refName, *pos)))
+
+
+		#update listwidget
+	def loadList(self):
+		fn = QtWidgets.QFileDialog.getOpenFileName(self, 'Open pointlist', self.saveLoc)[0]
+		if fn:
+			try:
+				spc = pd.read_csv(fn, sep = '\t', header = None)
+				if spc.shape[1] != self.dim:
+					print("Need {} columns.".format(self.dim))
+					return
+				#we need to make pairs x,y here
+				for i in range(len(spc[0])):
+					self.addToList('',(spc[0][i],spc[1][i]))
+			except:
+				print("Hmmm..can't open this")
