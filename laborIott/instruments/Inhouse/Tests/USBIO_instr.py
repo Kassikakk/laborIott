83a88389820bd8d9906859ea511e234667138678
Kassikakk <kleiger@ut.ee>
2025-08-19 15:15:46 +0300

    USBIO second PWM

--- laborIott/instruments/Inhouse/Tests/USBIO_instr.py
+++ laborIott/instruments/Inhouse/Tests/USBIO_instr.py
@@ -3,11 +3,12 @@ from laborIott.adapters.USBAdapter import USBAdapter
 from time import sleep
 
 usbio = USBIO(USBAdapter(0xcacc, 0x0004))
-'''
-usbio.freq2 = 100
+
+usbio.freq2 = 50
 usbio.duty2 = 3000
 sleep(1)
-usbio.duty2 = 700
+usbio.duty2 = 1000
+'''
 usbio.freq2 = 0
 
 usbio.freq1 = 100
@@ -19,12 +20,14 @@ usbio.duty1 = 1000
 sleep(1)
 usbio.duty1 = 2000
 #usbio.freq2 = 0
-'''
+
 usbio.freq1 = 300
 usbio.OD = 2000
-
-sleep(2)
+'''
 print(usbio.freq1)
 usbio.freq1 = 0
+sleep(2)
 
+
+print(usbio.freq1)
 usbio.setpin(1,1)
