f8539417891b1948e8be6bbe2efd59aa4482f1a7
Kassikakk <kleiger@ut.ee>
2025-11-21 01:23:01 +0200

    KymeraVI adjusted

--- laborIott/VInst/Andor/KymeraVI.py
+++ laborIott/VInst/Andor/KymeraVI.py
@@ -1,19 +1,26 @@
-from laborIott.instruments.Andor.VInst.AndorVI import Andor_VI
+from laborIott.VInst.Andor.iDusVI import iDus_VI
 from laborIott.adapters.SDKAdapter import SDKAdapter
-from laborIott.adapters.ZMQAdapter import ZMQAdapter
+#from laborIott.adapters.ZMQAdapter import ZMQAdapter
 from laborIott.instruments.Andor.Inst.kymera import IDusKymera
 from PyQt5 import QtWidgets
-from laborIott.instruments.Andor.VInst.KymeraDlg import Ui_KymeraDialog #pyuic5 generated dialog
+from laborIott.VInst.Andor.KymeraDlg import Ui_KymeraDialog #pyuic5 generated dialog
 import os, sys
 
 
 def localPath(filename):
 	return os.path.join(os.path.dirname(os.path.abspath(__file__)),filename)
 
-class AndorKymera_VI(Andor_VI):
+'''
+In this case we are dealing with the I2C connected spectrometer, so basically 
+there is just 1 instrument, the camera
+'''
+
+class AndorKymera_VI(iDus_VI):
 	
-	def __init__(self, refname = "iDus"):
-		super(AndorKymera_VI, self).__init__(refname)
+	def __init__(self):
+		super().__init__("Kymera",IDusKymera,SDKAdapter("atmcd32d_legacy",False))
+		
+
 		self.dlg = QtWidgets.QDialog()
 		self.kymDlg = Ui_KymeraDialog()
 		self.kymDlg.setupUi(self.dlg)
@@ -23,16 +30,16 @@ class AndorKymera_VI(Andor_VI):
 		self.spectromButt.setEnabled(True)
 		
 		#initialize self.kymDlg 4 fields
-		self.kymDlg.cWlEdit.setText("%.3f" % self.idus.centerpos)
-		self.kymDlg.focusEdit.setText("%d" % self.idus.focus)
+		self.kymDlg.cWlEdit.setText("%.3f" % self.instrum.centerpos)
+		self.kymDlg.focusEdit.setText("%d" % self.instrum.focus)
 		
-		fdict = self.idus.filterdict
+		fdict = self.instrum.filterdict
 		self.kymDlg.filterCombo.addItems(list(fdict.keys()))
-		self.kymDlg.filterCombo.setCurrentText(list(fdict.keys())[list(fdict.values()).index(self.idus.filter)])
+		self.kymDlg.filterCombo.setCurrentText(list(fdict.keys())[list(fdict.values()).index(self.instrum.filter)])
 		
-		gdict = self.idus.gratingdict
+		gdict = self.instrum.gratingdict
 		self.kymDlg.gratingCombo.addItems(list(gdict.keys()))
-		self.kymDlg.gratingCombo.setCurrentText(list(gdict.keys())[list(gdict.values()).index(self.idus.grating)])
+		self.kymDlg.gratingCombo.setCurrentText(list(gdict.keys())[list(gdict.values()).index(self.instrum.grating)])
 		
 		self.spectromButt.clicked.connect(self.dlg.show) #_exec annab modaalse
 		
@@ -41,14 +48,6 @@ class AndorKymera_VI(Andor_VI):
 		self.kymDlg.filterCombo.currentIndexChanged.connect(self.setFilter)
 		self.kymDlg.gratingCombo.currentIndexChanged.connect(self.setGrating)
 	
-	
-	def connectInstr(self, refname):
-		#overridden for IDusKymera
-		adapter = self.getZMQAdapter(refname)
-		if adapter is None:
-			adapter = SDKAdapter(localPath("../Inst/atmcd32d_legacy"),False)
-		self.idus = IDusKymera(adapter)
-
 		
 	
 		
@@ -59,10 +58,10 @@ class AndorKymera_VI(Andor_VI):
 		except ValueError:
 			print("not floatable")
 			return 
-		self.idus.centerpos = value
-		self.kymDlg.cWlEdit.setText("%.3f" % self.idus.centerpos)
+		self.instrum.centerpos = value
+		self.kymDlg.cWlEdit.setText("%.3f" % self.instrum.centerpos)
 		#also change wavelength scale
-		self.xdata = self.idus.wavelengths
+		self.xdata = self.instrum.wavelengths
 		
 	def setFocus(self, value):
 		#try to convert to int
@@ -71,24 +70,24 @@ class AndorKymera_VI(Andor_VI):
 		except ValueError:
 			print("not intable")
 			return 
-		self.idus.focus = value
-		self.kymDlg.focusEdit.setText("%d" % self.idus.focus)
+		self.instrum.focus = value
+		self.kymDlg.focusEdit.setText("%d" % self.instrum.focus)
 		
 	def setFilter(self, i):
 		#note that i here is combobox index, not the filter number (which should mostly be i+1)!
 		#hence for any outside call, this function needs to be modified
-		filtno = self.idus.filterdict[self.kymDlg.filterCombo.currentText()]
-		self.idus.filter = filtno
+		filtno = self.instrum.filterdict[self.kymDlg.filterCombo.currentText()]
+		self.instrum.filter = filtno
 		
 	def setGrating(self, i):
 		#i is combobox index again
 		#hence for any outside call, this function needs to be modified
-		gratno = self.idus.gratingdict[self.kymDlg.gratingCombo.currentText()]
-		self.idus.grating = gratno
+		gratno = self.instrum.gratingdict[self.kymDlg.gratingCombo.currentText()]
+		self.instrum.grating = gratno
 		#the focus also seems to change when grating is changed
-		self.kymDlg.focusEdit.setText("%d" % self.idus.focus)
+		self.kymDlg.focusEdit.setText("%d" % self.instrum.focus)
 		#also change wavelength scale
-		self.xdata = self.idus.wavelengths
+		self.xdata = self.instrum.wavelengths
 		
 		
 		
